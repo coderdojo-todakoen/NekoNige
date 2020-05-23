da847bbb9f8f59a9de60dff954cccaf0dbf5597d
coderdojo-todakoen <toda.jp@coderdojo.com>
2020-05-23 12:03:01 +0900

    新規作成

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    // 移動する速さ
+    public float speed = 2;
+
+    // Animatorコンポーネントを取得します
+    private Animator animator;
+
+    // 移動が開始された(何かキーが押された)かどうか
+    private bool start;
+
+    // 壁にぶつかり向きを変えている間trueにします
+    private bool turning;
+
+    // 向きを変える際の中心の座標
+    private Vector3 center;
+    // 向きを変える際の中心軸
+    private Vector3 axis;
+    // 向きを変える際の経過時間
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Animatorコンポーネントを取得します
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 何かキーが押されたらスタートします
+        if (!start)
+        {
+            // まだスタートしていない時に、何かキーが
+            // 押されたら
+            if (Input.anyKeyDown)
+            {
+                // 歩くアニメーションを開始して
+                // スタートします
+                animator.SetBool("walk", true);
+                start = true;
+            }
+            return;
+        }
+
+        if (animator.GetBool("walk"))
+        {
+            // 歩くアニメーション中は移動します
+            Move();
+        }
+    }
+
+    // ネコの移動をおこないます
+    // 壁にぶつかると向きを変えながら前へ進みます
+    private void Move()
+    {
+        // 壁にぶつかり向きを変えている途中かどうかをチェックします
+        if (!turning)
+        {
+            // 壁にぶつかっていなければ、前に移動します
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else
+        {
+            // 向きを変えている場合は、経過時間をチェックします
+            if (time <= 1)
+            {
+                // 中心点、中心軸の周りを1秒間で90度回転します
+                transform.RotateAround(center, axis, 90 * Time.deltaTime);
+                time += Time.deltaTime;
+            }
+            else
+            {
+                // 1秒以上経過したら向きを変えるのを停めます
+                turning = false;
+            }
+        }
+    }
+
+    // 右周りに向きを変えるように回転軸を設定します
+    private void PrepareTurnRight()
+    {
+        axis = Vector3.up;
+    }
+
+    // 左周りに向きを変えるように回転軸を設定します
+    private void PrepareTurnLeft()
+    {
+        axis = Vector3.down;
+    }
+
+    // 壁にぶつかったため、Update()時に向きを変えるよう設定します
+    private void PrepareTurn(bool right)
+    {
+        if (turning)
+        {
+            // 向きを変えている間は、さらに壁にぶつかっても
+            // 何もしません。(現在の向きを変える処理を続けます)
+            return;
+        }
+
+        if (right)
+        {
+            // 右周りに向きを変えるように回転軸を設定します
+            PrepareTurnRight();
+        }
+        else
+        {
+            // 左周りに向きを変えるように回転軸を設定します
+            PrepareTurnLeft();
+        }
+        // 向きを変える際の中心座標を設定します
+        // ネコの現在の中心座標をワールド座標に変換したものです
+        center = transform.TransformPoint(Vector3.zero);
+        // 向きを変える際の経過時間を設定します
+        time = 0;
+        // 次のUpdate()から向きを変えるようにします
+        turning = true;
+    }
+
+    // 衝突を検出した場合に呼び出されます
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "NorthWall":
+                // 壁にぶつかった場合、向きを変えるようにします
+                PrepareTurn(transform.forward.x > 0);
+                break;
+            case "SouthWall":
+                // 壁にぶつかった場合、向きを変えるようにします
+                PrepareTurn(transform.forward.x < 0);
+                break;
+            case "EastWall":
+                // 壁にぶつかった場合、向きを変えるようにします
+                PrepareTurn(transform.forward.z < 0);
+                break;
+            case "WestWall":
+                // 壁にぶつかった場合、向きを変えるようにします
+                PrepareTurn(transform.forward.z > 0);
+                break;
+            case "Mouse":
+                // ネズミにぶつかった場合、移動を停めて
+                // 次のアニメーションを開始します
+                animator.SetBool("walk", false);
+
+                // 効果音を再生します
+                GetComponent<AudioSource>().PlayDelayed(0.6F);
+                break;
+            default:
+                return;
+        }
+    }
+}
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Mouse : MonoBehaviour
+{
+    // 移動する速さ
+    public float speed = 10.0f;
+    // 回転する速さ
+    public float rotationSpeed = 100.0f;
+
+    // キー入力で移動可能かどうか
+    // ネコにぶつかった後、moveはfalseになります
+    private bool move = true;
+    // ネコにぶつかった際のネズミの位置
+    private Vector3 fromPosition;
+    // ネコにぶつかった後の移動先
+    private Vector3 downPosition;
+    // ネコにぶつかった後のカメラの方向
+    private Vector3 lookPosition;
+    // ネコにぶつかった後の経過時間
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            // キー入力による移動をおこないます
+            Move();
+        }
+        else
+        {
+            // ネコにぶつかった後の移動をおこないます
+            FallDown();
+        }
+    }
+
+    // ネズミの移動をおこないます
+    // キー入力に応じて、前後への移動と、左右への回転をおこないます
+    private void Move()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+
+    // ネコにぶつかった後の移動をおこないます
+    private void FallDown()
+    {
+        if (time < 5)
+        {
+            // ネコの正面に移動します
+            transform.position = Vector3.Slerp(fromPosition, downPosition, time);
+
+            // ネコの方向を向きます
+            transform.LookAt(lookPosition, Vector3.up);
+        }
+        else if (time < 7)
+        {
+            // 5秒経過後、7秒まで暗転します
+            GameObject panel = GameObject.Find("Panel");
+            Image image = panel.GetComponent<Image>();
+            image.color = new Color(0, 0, 0, Mathf.Max(Mathf.Min((time - 5) / 2, 1), 0));
+        }
+        else if (time < 8)
+        {
+            // シーンを読み込み直します
+            SceneManager.LoadScene("Main");
+        }
+
+        time += Time.deltaTime;
+    }
+
+    // ネコにぶつかった場合の処理をおこないます
+    private void PrepareDown(GameObject cat)
+    {
+        // キー入力による移動を停めます
+        move = false;
+
+        // ぶつかった際のネズミの位置を取得します
+        fromPosition = transform.position;
+
+        // ぶつかった際のネコの位置を取得し、その正面の座標を移動先とします
+        downPosition = cat.transform.position + cat.transform.forward;
+        // ネコの下に位置するようにy座標を小さくします
+        downPosition.y = -0.2F;
+
+        // ぶつかった際のネコの位置を取得し、その正面の座標をカメラの方向とします
+        lookPosition = cat.transform.position + cat.transform.forward * 0.2F;
+        // ネコの顔に視線が向くように座標を調整します
+        lookPosition.y = 0.5F;
+    }
+
+    // 衝突を検出した場合に呼び出されます
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Cat":
+                // ネコにぶつかったので、移動を停めます
+                PrepareDown(other.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
